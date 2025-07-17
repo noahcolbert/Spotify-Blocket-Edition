263dd574d825a5a4a9f17ec24694877d721fcb49
Noah Colbert <noah@noahcolbert.com>
2025-07-17 16:25:08 -0400

    Quick add to update versioning and prepare the Controller item to activate a menu on click. Will come back when I know how to make a menu.

--- src/main/java/com/thusiast/spotifybe/Controller.java
+++ src/main/java/com/thusiast/spotifybe/Controller.java
@@ -2,17 +2,32 @@ package com.thusiast.spotifybe;
 
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 import java.util.function.Function;
 
-public class Controller {
+public class Controller extends Item {
+
+    public Controller(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand){
+        //TODO: Update this to open Spotify menu when menu logic is added.
+        return ActionResult.PASS;
+    }
+
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create key.
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SpotifyBlocketEdition.MOD_ID, name));
