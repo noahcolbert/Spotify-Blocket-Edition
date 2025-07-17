f1dbc69d149fc1be07f1ad7dca6ba729b95d9e74
Noah Colbert <noah@noahcolbert.com>
2025-07-17 13:21:45 -0400

    Began project, implemented basic item logic

---
+++ src/client/java/com/thusiast/spotifybe/SpotifyBlocketEditionClient.java
@@ -0,0 +1,10 @@
+package com.thusiast.spotifybe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpotifyBlocketEditionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/thusiast/spotifybe/Controller.java
@@ -0,0 +1,33 @@
+package com.thusiast.spotifybe;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class Controller {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SpotifyBlocketEdition.MOD_ID, name));
+
+        // Create instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static final Item CONTROLLER = register("controller", Item::new, new Item.Settings());
+
+    public static void initialize() {
+        //dummy func
+    }
+
+}
